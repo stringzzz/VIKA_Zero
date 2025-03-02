9086f56e9b3c5b2bbf0faa4c69aa7aaa63a61689
stringzzz <119706411+stringzzz@users.noreply.github.com>
2025-03-01 17:09:19 -0800

    Fixed mistake in split + Other mistake

--- VZCA_Further_Tests/VZ_sample_statistics_class/distribution_converter.py
+++ VZCA_Further_Tests/VZ_sample_statistics_class/distribution_converter.py
@@ -2,5 +2,5 @@
 with open("KAE_Distribution1.txt", 'r') as distribution_file:
     with open("KAE_Distribution1_converted.txt", 'w') as converted_file:
         for line in distribution_file.readlines():
-            temp_x, temp_y = line.split(":\t")
-            converted_file.write(f"{temp_x}: {int(float(temp_y.strip()))}\n")
+            temp_x, temp_y = line.split(" ")
+            converted_file.write(f"{temp_x} {int(float(temp_y.strip()))}\n")
